f9b4e28a309e8187ea0c0296a689a4f733d57d7d
Shalom Sims <35307624+Zuzuni7@users.noreply.github.com>
2019-10-11 22:29:10 -0600

    Update Test.java

--- src/Nixon/Test.java
+++ src/Nixon/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args)
     {
         System.out.println("Hello, my name is Dakota");
+        System.out.println("Hola Dakota! Soy Shalom. What pasa?");
     }
 
 }
